280b9f8c5aa5daa466cd9e80fe7266fa9ad7c0d2
Gjermund Skobba <gjermund@skobba.net>
2021-11-19 13:29:57 +0100

    Add Martin endpoint

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+app.use('/martin', (req, res) => {
+
+  res.json({"navn" : "Martin"})
+});
+
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
